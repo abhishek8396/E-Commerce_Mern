bfca5fe5ae2d59e5c4f9dfffee562ad2f1d46c23
Abhishek <113371594+abhishek8396@users.noreply.github.com>
2023-07-31 10:46:00 +0530

    Update server.js
    new update

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
-import express from "express"
-import path from 'path'
-import cors from "cors"
-import dotenv from "dotenv"
-import connectDB from "./Connection/database.js"
-import morgan from "morgan"
-import authRoute from "./routes/authRoute.js"
-import categoryRoute from './routes/categoryRoute.js'
-import productRoute from './routes/productRoute.js'
+import express from "express";
+import path from 'path';
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./Connection/database.js";
+import morgan from "morgan";
+import authRoute from "./routes/authRoute.js";
+import categoryRoute from './routes/categoryRoute.js';
+import productRoute from './routes/productRoute.js';
 
-const app= express(); 
+const app = express();
 //configures env
 dotenv.config();
 // const PORT = process.env.PORT || 4500;
 app.use(cors());
 //Middleware
 app.use(express.json());
-app.use(morgan('dev'))
-
+app.use(morgan('dev'));
 
 //DataBase Connect
 connectDB();
@@ -28,13 +27,17 @@ app.use('/api/v1/category', categoryRoute);
 //Routes for Product
 app.use('/api/v1/product', productRoute);
 
+// Static files (e.g., React app) should be served using the absolute path, not __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(express.static(path.join(__dirname, './ecommerce/build')));
 
-app.get('*', function(req,res){
-    res.sendFile(path.join(__dirname, './ecommerce/build/index.html'))
-})
-const PORT = 8080;
-app.listen(PORT, ()=>{
-    console.log(`Server is running at https://localhost:${PORT}`.bgRed.white)
-})
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, './ecommerce/build/index.html'));
+});
 
+const PORT = 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running at https://localhost:${PORT}`);
+});
